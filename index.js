9c520c873b6b831fc994b0bf88e692b870e85f95
M <muzameelahmed1@gmail.com>
2023-10-30 13:08:42 +0000

    async API function

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-require('dotenv').config();
+async function getImage(query) {
+  let endpoint = `https://api.giphy.com/v1/gifs/search?api_key=MGL1odT9huYYFRBEBTe2bJ1gI9bccWaJ&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+  const randomNum = Math.floor(Math.random() * 25);
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  console.log(data.data[randomNum].url);
+}
 
-// Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+getImage("cat");
